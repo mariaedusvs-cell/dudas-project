44d22ebf811fdc8b868a6800b10711deb3c773d5
unknown <Aluno@CNIT-LAB01-PC36.intranet.ifrj.edu.br>
2026-04-10 09:03:16 -0300

    dudinha

--- Assets/lixocontrole.cs
+++ Assets/lixocontrole.cs
@@ -19,6 +19,14 @@ public class lixocontrole : MonoBehaviour
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         this.transform.position += new Vector3 (horizontalInput*velocity,0,0);
+        if (this.transform.position.x>10)
+        {
+          this.transform.position = new Vector3(10,this.transform.position.y,this.transform.position.z);
+
+        }else if (this.transform.position.x<-10)
+        {
+          this.transform.position = new Vector3(-10,this.transform.position.y,this.transform.position.z);
+        }
 
     }
 }
