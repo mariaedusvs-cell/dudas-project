42bbcef90893a12753c05c380698528fa09d5db1
unknown <mpilar.ribeiro20@gmail.com>
2026-05-12 17:23:11 -0300

    savha

--- Assets/AddPoints.cs
+++ Assets/AddPoints.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AddPoints : MonoBehaviour
-{ 
+{
+    public AudioSource source;
     public LixoSpawnerController lixoSpawnerController;
     private void OnCollisionEnter(Collision collision ){
         if (collision.gameObject.CompareTag("Lixo")){
             Destroy(collision.gameObject);
+            source.Play();
             lixoSpawnerController.AddToPoints(1);
         }
     }
