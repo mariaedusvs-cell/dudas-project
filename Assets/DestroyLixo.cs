8abb1210fac2d92cc15ef16900f34392bbd7fb2e
thiagonogueira111 <mariaedu.svs@gmail.com>
2026-05-15 09:12:24 -0300

    drgrt

--- Assets/DestroyLixo.cs
+++ Assets/DestroyLixo.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class DestroyLixo : MonoBehaviour
 {
+    public LixoSpawnerController lixoSpawnerController;
      private void OnCollisionEnter(Collision collision) {
         // Optional: Check if the entering object is the Player
         if (collision.gameObject.CompareTag("Lixo")) {
             Destroy(collision.gameObject);
+            if (lixoSpawnerController.points > 0)
+            {
+                lixoSpawnerController.AddToPoints(-1);
+            }
         }
     }
 }
